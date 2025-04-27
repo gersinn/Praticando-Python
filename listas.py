e48591ba2222d4e90142500ece5d74133af0f9cf
unknown <gersonalmeida399@gmail.com>
2025-04-27 12:29:15 -0300

    iniciando repositorio

---
+++ listas.py
@@ -0,0 +1,18 @@
+
+
+def estudante():
+    estudante_lst = {
+        'Pedro': 24,
+        'Ronado': 22,
+        'Ana': 22,
+        'Janaina': 25    
+    }
+
+    estudante_lst.append ='Marcelo'
+
+    print(estudante_lst['Pedro'])
+
+
+
+
+estudante()
---
+++ while.py
@@ -0,0 +1,51 @@
+""" # Atividade em Python """
+import math
+# numero = 0
+
+
+""" 
+while numero < 10:
+    numero += 1
+    print(numero + 1) """
+    
+    
+
+""" while numero < 100:
+    numero += 1
+    print (numero + 1, numero + 1) """
+    
+# while numero < 500:
+#     numero +=1
+#     print(numero)
+    
+    
+
+
+def Pares():
+    numero = 0
+    while numero < 10:
+        numero += 1
+        if numero % 2 == 0 :
+            print(numero)
+
+
+# def inpar():                
+#    par = 0
+#    inpar = 0
+    
+# for num in lista:
+#         if (num %2 ) = 0
+#             par = par + 1
+#         else:
+#             impar = impar + 1
+#     return impar
+
+
+
+
+# inpar()
+
+
+# divisao = 2 % 2
+
+# print(divisao)
